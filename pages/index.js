45cdcc2ee1093d5d5c1d9d8dedf47468da5faa25
Jared Borders <jared.borders@egov.com>
2021-02-09 21:54:22 -0600

    Create Linktr.ee-esque app

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>jaredborders</title>
+			</Head>
+			<main className={styles.container}>
+				<img
+					className={styles.image}
+					src="https://avatars.githubusercontent.com/u/44350516?s=460&u=f4a22b0c1d191bb30d141e579baec86c2ce67a9e&v=4"
+				/>
+				<h3 className={styles.description}>@jaredborders</h3>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+				<div className={styles.grid}>
+					<a
+						href="https://read.cv/jaredborders"
+						className={styles.card}
+					>
+						<h3>read.cv</h3>
+					</a>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+					<a
+						href="https://github.com/JaredBorders"
+						className={styles.card}
+					>
+						<h3>Github</h3>
+					</a>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+					<a
+						href="https://twitter.com/Jared_Borders"
+						className={styles.card}
+					>
+						<h3>Twitter</h3>
+					</a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+					<a
+						href="https://soundcloud.com/yumuru"
+						className={styles.card}
+					>
+						<h3>Soundcloud</h3>
+					</a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+					<a
+						href="https://www.instagram.com/jared_borders"
+						className={styles.card}
+					>
+						<h3>Instagram</h3>
+					</a>
+				</div>
+			</main>
+		</div>
+	);
 }
